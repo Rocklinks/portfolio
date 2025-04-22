6bf296ed9b796d9b61e3be341f27fe89127dd638
Rocklin K S <rocklin2603@gmail.com>
2025-04-22 22:32:50 +0530

    new

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,8 +1,8 @@
 import himg from '../assets/hero.png'
 
 export default function Hero() {
-    return <section className='flex'>
-        <h1 className='w-1/2'>Hi, <br/> Im Rocklin K S
+    return <section className='flex px-5 py-32 bg-secondary justify-center'>
+        <h1 className='w-1/2 text-white text-4xl'>Hi, <br/> Im Rocklin K S
         <p>Im a Web-Developer</p>
         </h1>
         <img className='w-1/3' src={himg} alt="Rocky Bhai"/>
