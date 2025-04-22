60304901f23ef55acb694b3ca5594a4c5f40f519
Rocklin K S <rocklin2603@gmail.com>
2025-04-22 22:05:37 +0530

    img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Hero from './components/hero';
 
 function App() {
   return (
     <div className="App">
           <Header/>
+          <Hero/>
     </div>
   );
 }
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,5 +1,12 @@
+import himg from '../assets/hero.png'
+
 export default function Hero() {
     return <section>
-        <h1>HERO SECTION</h1>
+        <h1>Hi, <br/> Im Rocklin K S
+        <p>Im a Web-Developer</p>
+        </h1>
+        <img src={himg} alt="Rocky Bhai"/>
+        
+
     </section>
 }
